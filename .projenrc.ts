7c69e3864b8f153fbb145c4fc3282ca309c367c7
Todd Bryant <hello@toddaas.com>
2024-11-12 12:12:44 +1100

    initial commit

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,8 +3,8 @@ const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'autobot',
   projenrcTs: true,
-
-  // deps: [],                /* Runtime dependencies of this module. */
+  gitignore: ['.env'],
+  deps: ['dotenv', 'telegraf'], /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
-  }
+import * as dotenv from 'dotenv';
+import { Telegraf } from 'telegraf';
+
+dotenv.config({
+  path: '../.env',
+});
+const token = process.env.BOT_TOKEN;
+if (!token) {
+  throw new Error('BOT_TOKEN environment variable is not set');
 }
+
+const bot = new Telegraf(token);
+//console.log('Is the current bot token set', process.env.BOT_TOKEN);
+bot.command('oldschool', (ctx) => ctx.reply('breasts ( . )( . )'));
+bot.command('hipster', Telegraf.reply(':)'));
+void bot.launch();
+//console.log(bot);
+
+// // // Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
